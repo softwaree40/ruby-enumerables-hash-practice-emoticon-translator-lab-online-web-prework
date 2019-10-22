52425590b2bdac10cec1fd0fc9d8483fb6dae402
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-22 14:43:48 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,25 @@
-
+require  "pry"
 require "yaml"
+
+
 def load_library(file)
    emoticons = YAML.load_file("./lib/emoticons.yml")
    emoticons
+   new = Hash.new
+   if !new.has_key?('get_meaning')
+      new['get_meaning'] = {}
+   # binding.pry
+   end
+   if !new.has_key?('get_emoticon')
+     new['get_emoticon'] = {}
+   end
+   emoticons.each do |key,value|
+     new['get_meaning'][value[1]] = key
+     new['get_emoticon'][value[0]] = key
+     
+    # binding.pry 
+   end
+   return new
 end
 
 def get_japanese_emoticon
